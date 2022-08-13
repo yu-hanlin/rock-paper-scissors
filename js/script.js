7a6998fc8f68d3a23ae19c664cd5902535211225
Hanlin Yu <yuhanlin611@gmail.com>
2022-08-13 15:17:11 -0700

    Complete basic functionality of game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let temp = Math.floor(Math.random() * 3);
-    switch(temp) {
+    switch (temp) {
         case 0:
             return "Rock";
         case 1:
@@ -10,4 +10,61 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "Rock":
+                return 0;
+            case "Paper":
+                return -1;
+            case "Scissors":
+                return 1;
+        }
+    }
+    else if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "Rock":
+                return 1;
+            case "Paper":
+                return 0;
+            case "Scissors":
+                return -1;
+        }
+    }
+    else {
+        switch (computerSelection) {
+            case "Rock":
+                return -1;
+            case "Paper":
+                return 1;
+            case "Scissors":
+                return 0;
+        }
+    }
+}
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    let playerSelection = "";
+    let computerSelection = "";
+    let result = 0;
+    for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Choose rock, paper, or scissors");
+        console.log("Your choice: " + playerSelection);
+        console.log("Computers choice: " + computerSelection);
+        result = playRound(playerSelection, computerSelection);
+        if (result == 1) {
+            userScore += result;
+        }
+        else if (result == -1) {
+            computerScore -= result;
+        }
+    }
+    return "User score: " + userScore + " | Computer score: " + computerScore;
+}
+
+console.log(game());
